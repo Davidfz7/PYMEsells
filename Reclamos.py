91ab43a943a967c7ebf4e36fc897b8669af32eb7
Chakka28 <30502538+Chakka28@users.noreply.github.com>
2022-08-21 00:20:07 -0600

    Update de prueba commit

--- Reclamos.py
+++ Reclamos.py
@@ -134,6 +134,6 @@ def eliminarChar(comentarios):
     for key, value in char_to_replace.items():
         comentarios = comentarios.replace(key,value)
     return comentarios
-print('Hola')
+print('david loca')
 print('Adios')
 PrimerMenu()
